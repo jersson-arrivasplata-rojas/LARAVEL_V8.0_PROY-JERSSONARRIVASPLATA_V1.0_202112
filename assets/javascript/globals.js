35b73fcaad22c91a7de30c5177f497880146aa73
Jersson Arrivasplata Rojas <developer@jerssonarrivasplata.com>
2021-12-26 16:51:15 -0500

    Actualización de Configuraciones
    Actualización de Configuraciones

--- assets/javascript/globals.js
+++ assets/javascript/globals.js
@@ -1,12 +1,23 @@
-function menu(){
-    //let d = new Date();
-    //document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
-    console.log(document.getElementsByTagName('header'))
-    if(document.getElementsByTagName('header')[0].classList.contains('header-close')==true){
-        document.getElementsByTagName('header')[0].classList.remove('header-close')
-    }else{
-        document.getElementsByTagName('header')[0].classList.add('header-close')
+(function() {
+    let menu = function() {
+        const headerClass = document.getElementsByTagName('header')[0].classList;
+        const classVerify = 'header-close';
+        headerClass.contains(classVerify) ? headerClass.remove(classVerify) : headerClass.add(classVerify);
+    };
+
+    let loaderWithHash = function() {
+        if (location.pathname !== '/') {
+            loader();
+        }
+
+    };
+    let loaderWithOutHash = function() {
+        loader();
+
+    };
+    let loader = function() {
+        const loaderClass = document.getElementsByClassName('loader')[0].classList;
+        const classVerify = 'd-none';
+        if (loaderClass.contains(classVerify)) loaderClass.remove(classVerify);
     }
-}
-//let d = new Date();
-//document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+})();
